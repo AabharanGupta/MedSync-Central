698b4543dc26a075536283ec03e3c5bb7e86a1c9
adityasingh1389 <111131296+adityasingh1389@users.noreply.github.com>
2024-11-12 10:32:16 +0530

    Update App.jsx
    update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import NavBar from "../components/Navbar.jsx";
+import Hero from "../components/Hero.jsx";
+import heropic from "./assets/heropic.png";
+import Menu from "../components/Menu.jsx";
+import "./App.css";
 export default function App() {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <Hero></Hero>
+        <img src={heropic} alt="heropic" className="heropic" />
+      </div>
+      <Menu></Menu>
+    </>
+  );
 }
+
