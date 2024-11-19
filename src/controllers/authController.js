f93c6500619e2691649d503f16ae39c19f28b428
Aabharan <aabharangupta1001@gmail.com>
2024-11-19 22:24:54 +0530

    auth backend complete

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -75,9 +75,37 @@ export const setup2FA= async(req,res)=>{
 }
 
 export const verify2FA= async(req,res)=>{
-    
+    const {token}=req.body;
+    const user=req.user;
+
+    const verified=speakeasy.totp.verify({           //for setting timed otp
+        secret: user.twoFactorSecret,
+        encoding:"base32",
+        token,
+    });
+    if(verified){
+        const jwtToken=jwt.sign(
+            {username:user.username},
+            process.env.JWT_SECRET,
+            {expiresIn:"1hr"}
+        );
+        res.status(200).json({
+            message:"2FA successful",
+            token:jwtToken
+        });
+    }else{
+        res.status(400).json({message:"Invalid 2FA Token"})
+    }                   
 }
 
 export const reset2FA= async(req,res)=>{
-    
+    try{
+        const user=req.user;
+        user.twoFactorSecret="";
+        user.isMfaActive=false;
+        await user.save();
+        res.status(200).json({message:"2FA reset successful"});
+    }catch(error){
+        res.status(500).json({error:"Errror reseting 2FA",message:error})
+    }
 }
